ecb77efcab5684613855f38916fd53eecc79ec4a
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-25 12:06:03 +0000

    make a tip calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,61 @@
 import { useState } from "react";
-import "./styles.css";
-
-const faqs = [
-  {
-    title: "Where are these chairs assembled?",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
-  },
-  {
-    title: "How long do I have to return my chair?",
-    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
-  },
-  {
-    title: "Do you ship to countries outside the EU?",
-    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
-  },
-];
 
 export default function App() {
+  const [bill, setBill] = useState(0);
+  const [tip1, setTip1] = useState(0);
+  const [tip2, setTip2] = useState(0);
+  let totalTip = (((+tip1 + +tip2) / 2) * Number(bill)) / 100;
+  function handleReset() {
+    setBill(0);
+    setTip1(0);
+    setTip2(0);
+  }
+
   return (
     <div>
-      <Accordion data={faqs} />
+      <Bill bill={bill} setBill={setBill} />
+      <ServiceLevel tip={tip1} setTip={setTip1}>
+        How did you like the service?
+      </ServiceLevel>
+      <ServiceLevel tip={tip2} setTip={setTip2}>
+        How did your friend like the service?
+      </ServiceLevel>
+      <Total bill={bill} totalTip={totalTip} onReset={handleReset} />
+    </div>
+  );
+}
+function Bill({ bill, setBill }) {
+  return (
+    <label>
+      How much was the bill?{" "}
+      <input
+        type="number"
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
+    </label>
+  );
+}
+function ServiceLevel({ children, tip, setTip }) {
+  return (
+    <div>
+      <span>{children}</span>
+      <select onChange={(e) => setTip(e.target.value)} value={tip}>
+        <option value="0">Dissatisfied(0%)</option>
+        <option value="5">It was okay(5%)</option>
+        <option value="10">It was good(10%)</option>
+        <option value="20">Absolutely amazing!(20%)</option>
+      </select>
     </div>
   );
 }
 
-function Accordion({ data }) {
-  const [curOpen, setCurOpen] = useState(null);
+function Total({ bill, totalTip, onReset }) {
+  let total = Number(bill) + Number(totalTip);
   return (
-    <div className="accordion">
-      {data.map((el, i) => (
-        <AccordionItem
-          curOpen={curOpen}
-          onOpen={setCurOpen}
-          title={el.title}
-          num={i + 1}
-          key={el.title}
-        >
-          {el.text}
-        </AccordionItem>
-      ))}
-    </div>
-  );
-}
-function AccordionItem({ num, title, curOpen, onOpen, children }) {
-  const isOpen = num === curOpen;
-  const handleToggle = () => {
-    onOpen(isOpen ? null : num);
-  };
-
-  return (
-    <div className={`item${isOpen ? " open" : ""}`} onClick={handleToggle}>
-      <p className="number">{num < 9 ? `0${num}` : num}</p>
-      <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
-
-      {isOpen && <div className="content-box">{children}</div>}
+    <div>
+      <p>{`You pay ${total} (${bill}+${totalTip})`}</p>
+      <button onClick={onReset}>Reset</button>
     </div>
   );
 }
